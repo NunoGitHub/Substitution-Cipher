e03eb84b4170ed4ca83321c64d4171cd42d765ca
Nuno Afonso Araújo Pereira <nunoaapp22@hotmail.com>
2020-11-27 11:17:11 +0000

    update

--- main.py
+++ main.py
@@ -6,24 +6,13 @@ import string
 import operator
 from collections import Counter
 
-def vigenere_decrypt(encrypted_vigener, keyword):
-    keyword_length = len(keyword)
-    keyword_as_int = [ord(i) for i in keyword]
-    encrypted_vigener_int = [ord(i) for i in encrypted_vigener]
-    plaintext = ''
-    for i in range(len(encrypted_vigener_int)):
-        if encrypted_vigener[i].isalpha():
-            value = (encrypted_vigener_int[i] - keyword_as_int[i % keyword_length]) % 26
-            plaintext += chr(value + 65)
-        else:
-            plaintext += encrypted_vigener[i]
-    return plaintext
 
-def Tenta2():
+def SubstitutionCipher():
     alphabet = dict.fromkeys(string.ascii_uppercase, 0)
     nWords = 0
     cipherText = "HG UOVI UJJXUTV VI GI BX, NRG H UB CIG USUTX GQUG UCA MIBBRCHGA QUV U THKQG GI ZITMX UCIGQXT GI NX MHFHOHVXL. VI OICK UV GQX VRZZXTXTV NA GQX NUL OUS LI CIG HCFIPX UVVHVGUCMX ZTIB IGQXT MIBBRCHGHXV, H MUCCIG ULBHG GQUG JXTVICV XCGHTXOA RCMICCXMGXL SHGQ GQXB IRKQG GI VGXJ HC UCL TXYRHTX GQUG U MICLHGHIC IZ GQHCKV SHGQ SQHMQ UOO SQI UTX LHTXMGOA HCGXTXVGXL UJJXUT GI NX VUGHVZHXL, VQIROL NX JRG UC XCL GI NXMURVX HG HV U VMUCLUO GI JXTVICV VIBX GQIRVUCLV IZ BHOXV LHVGUCG, SQI QUFX CI JUTG IT MICMXTC HC HG. OXG GQXB VXCL BHVVHICUTHXV, HZ GQXA JOXUVX, GI JTXUMQ UKUHCVG HG; UCL OXG GQXB, NA UCA ZUHT BXUCV (IZ SQHMQ VHOXCMHCK GQX GXUMQXTV HV CIG ICX), IJJIVX GQX JTIKTXVV IZ VHBHOUT LIMGTHCXV UBICK GQXHT ISC JXIJOX. HZ MHFHOHVUGHIC QUV KIG GQX NXGGXT IZ NUTNUTHVB SQXC NUTNUTHVB QUL GQX SITOL GI HGVXOZ, HG HV GII BRMQ GI JTIZXVV GI NX UZTUHL OXVG NUTNUTHVB, UZGXT QUFHCK NXXC ZUHTOA KIG RCLXT, VQIROL TXFHFX UCL MICYRXT MHFHOHVUGHIC. U MHFHOHVUGHIC GQUG MUC GQRV VRMMRBN GI HGV FUCYRHVQXL XCXBA, BRVG ZHTVG QUFX NXMIBX VI LXKXCXTUGX, GQUG CXHGQXT HGV UJJIHCGXL JTHXVGV UCL GXUMQXTV, CIT UCANILA XOVX, QUV GQX MUJUMHGA, IT SHOO GUPX GQX GTIRNOX, GI VGUCL RJ ZIT HG. HZ GQHV NX VI, GQX VIICXT VRMQ U MHFHOHVUGHIC TXMXHFXV CIGHMX GI YRHG, GQX NXGGXT. HG MUC ICOA KI IC ZTIB NUL GI SITVX, RCGHO LXVGTIAXL UCL TXKXCXTUGXL (OHPX GQX SXVGXTC XBJHTX) NA XCXTKXGHM NUTNUTHUCV."
     #cipherText ="HGUOVIUJJXUTVVIGIBXNRGHUBCIGUSUTXGQUGUCAMIBBRCHGAQUVUTHKQGGIZITMXUCIGQXTGINXMHFHOHVXLVIOICKUVGQXVRZZXTXTVNAGQXNULOUSLICIGHCFIPXUVVHVGUCMXZTIBIGQXTMIBBRCHGHXVHMUCCIGULBHGGQUGJXTVICVXCGHTXOARCMICCXMGXL2SHGQGQXBIRKQGGIVGXJHCUCLTXYRHTXGQUGUMICLHGHICIZGQHCKVSHGQSQHMQUOOSQIUTXLHTXMGOAHCGXTXVGXLUJJXUTGINXVUGHVZHXLVQIROLNXJRGUCXCLGINXMURVXHGHVUVMUCLUOGIJXTVICVVIBXGQIRVUCLVIZBHOXVLHVGUCGSQIQUFXCIJUTGITMICMXTCHCHGOXGGQXBVXCLBHVVHICUTHXVHZGQXAJOXUVXGIJTXUMQUKUHCVGHG;UCLOXGGQXBNAUCAZUHTBXUCVIZSQHMQVHOXCMHCKGQXGXUMQXTVHVCIGICXIJJIVXGQXJTIKTXVVIZVHBHOUTLIMGTHCXVUBICKGQXHTISCJXIJOXHZMHFHOHVUGHICQUVKIGGQXNXGGXTIZNUTNUTHVBSQXCNUTNUTHVBQULGQXSITOLGIHGVXOZHGHVGIIBRMQGIJTIZXVVGINXUZTUHLOXVGNUTNUTHVBUZGXTQUFHCKNXXCZUHTOAKIGRCLXTVQIROLTXFHFXUCLMICYRXTMHFHOHVUGHICUMHFHOHVUGHICGQUGMUCGQRVVRMMRBNGIHGVFUCYRHVQXLXCXBABRVGZHTVGQUFXNXMIBXVILXKXCXTUGXGQUGCXHGQXTHGVUJJIHCGXLJTHXVGVUCLGXUMQXTVCITUCANILAXOVXQUVGQXMUJUMHGAITSHOOGUPXGQXGTIRNOXGIVGUCLRJZITHGHZGQHVNXVIGQXVIICXTVRMQUMHFHOHVUGHICTXMXHFXVCIGHMXGIYRHGGQXNXGGXTHGMUCICOAKIICZTIBNULGISITVXRCGHOLXVGTIAXLUCLTXKXCXTUGXLOHPXGQXSXVGXTCXBJHTXNAXCXTKXGHMNUTNUTHUCV"
+    #contar o numero de letras do cipher text
     for i in cipherText:
         if i in alphabet:
             alphabet[i] = alphabet[i] + 1
@@ -32,6 +21,7 @@ def Tenta2():
     bigrams=[]
     #for i, letter in cipherText
    # print("The Bigrams Frequency is : " + str(dict(res)))
+    #dividir as palavras numa lista
     for i in cipherText.split():
         if i.isalpha():
             bigrams.append(i)
@@ -39,7 +29,7 @@ def Tenta2():
 
     trigrams=bigrams.copy()
     i=0
-    #delete
+    #criar remover todas as plavars com mais e menos de duas letras para criar bigramas
     while(len(bigrams)>i):
         if(len(bigrams[i]) >2  or len(bigrams[i]) <=1):
            bigrams.remove(bigrams[i])
@@ -49,15 +39,15 @@ def Tenta2():
     print(bigrams)
 
     auxBi = ""
-    # to remove duplicated
+    # to remove duplicated of bigrams
     # from list
     res = []
     for i in bigrams:
-        if i not in res:
+        if i not in res: #adiciona se não estiver na lista res
             res.append(i)
 
     print(res)
-
+    #trasnformar os bigramas num dicionario
     cpyBi2 = dict.fromkeys(res,0)
     print(cpyBi2)
 
@@ -73,6 +63,7 @@ def Tenta2():
     #print(sortedCipherBig)
 
     #trigrams
+    #  remover todas as plavars com mais e menos de 3 letras para criar trigramas
     i=0
     while (len(trigrams) > i):
         if (len(trigrams[i]) > 3 or len(trigrams[i]) < 3):
@@ -91,6 +82,7 @@ def Tenta2():
     print(removeDupli)
     #convert to dict
     cpytr = dict.fromkeys(removeDupli,0)
+    # count number of trigrams
     for i in trigrams:
         if i in cpytr:
             cpytr[i] = cpytr[i] + 1
@@ -114,5 +106,5 @@ def Tenta2():
 
 
 if __name__ == '__main__':
-    Tenta2()
+    SubstitutionCipher()
 
